9b69fd62bebbe8db2ed2c4c5560069d7df4077b7
Evelinabas0612 <evelinabas0612@gmail.com>
2023-05-01 06:56:08 +0300

    changed api_key

--- src/channel.py
+++ src/channel.py
@@ -5,7 +5,7 @@ from googleapiclient.discovery import build
 
 class Channel:
     """Класс для ютуб-канала"""
-    api_key = os.getenv('YT_API_KEY')
+    api_key = os.getenv('API_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
     print(api_key)
 
