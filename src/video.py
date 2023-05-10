f6d1beed9b7cb61de0be48aa5c8003fbb440e384
Evelinabas0612 <evelinabas0612@gmail.com>
2023-05-10 09:38:06 +0300

    added hw4

---
+++ src/video.py
@@ -0,0 +1,31 @@
+import os
+from googleapiclient.discovery import build
+
+api_key = os.getenv('API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+class Video:
+    """Класс для видео"""
+
+    def __init__(self, video_id: str) -> None:
+        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.video_id = video_id
+
+        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=video_id
+                                               ).execute()
+
+        self.id = video_response['items'][0]['id']
+        self.title = video_response['items'][0]['snippet']['title']
+        self.url = f"https://www.youtube.com/channel/{video_id}"
+        self.view_count = video_response['items'][0]['statistics']['viewCount']
+        self.like_count = video_response['items'][0]['statistics']['likeCount']
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class PLVideo(Video):
+    def __init__(self, video_id: str, playlist_id: str):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
